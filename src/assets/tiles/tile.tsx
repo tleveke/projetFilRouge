5b9b8e895ae88b42fc84991e091883f3d61ab20e
Leveque <levequetom77@gmail.com>
2021-01-08 16:50:36 +0100

    Remove Ts filev2

--- src/assets/tiles/tile.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tile" tilewidth="32" tileheight="32" tilecount="180" columns="20">
- <image source="../img/RPG Nature Tileset.png" width="641" height="288"/>
-</tileset>
