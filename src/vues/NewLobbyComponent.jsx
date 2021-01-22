d35250ee27c7927f034dfdc956f04f40f63a6a70
prynge <37028634+prynge@users.noreply.github.com>
2021-01-22 17:15:17 +0100

    Update NewLobbyComponent.jsx

--- src/vues/NewLobbyComponent.jsx
+++ src/vues/NewLobbyComponent.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { configGame } from '../game/config';
+import { TicTacToe } from '../game/Game';
 
 class NewLobbyComponent extends Component {
   constructor(props) {
@@ -10,7 +12,17 @@ class NewLobbyComponent extends Component {
     }
   }
 
-  createLobby = () =>{}
+  createLobby = (e) =>{
+    e.preventDefault()
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ numPlayers: this.state.number })
+    };
+    fetch(`${configGame.httpORs}://${configGame.urlServer}/games/Jeu_Fil_Rouge/create`, requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({name: data.matchID}));
+  }
 
   saveNameHandler = (event) => {
     this.setState({ name: event.target.value });
@@ -18,6 +30,19 @@ class NewLobbyComponent extends Component {
 
   saveNumberHandler = (event) => {
     this.setState({ number: event.target.value });
+  }
+  
+  handleNumberChange = (event) => {
+    this.setState({ number: event.target.value });
+  }
+
+  createSelect = () =>{
+    let select = []
+
+    for (let i = TicTacToe.minPlayers; i <= TicTacToe.maxPlayers; i++) {
+      select.push(<option key={i} value={i} >{i}</option>);   
+    }
+    return select;
   }
 
   componentDidMount() {
@@ -31,12 +56,16 @@ class NewLobbyComponent extends Component {
         <h2 className="text-center">Créer un Lobby</h2>
         <form className="form-signup col-10 mx-auto" >
           <div className="form-group">
-            <label>Nom du lobby</label>
-            <input type="text" name="name" className="form-control" placeholder="Nom du Lobby" value={this.state.name} onChange={this.saveNameHandler} />
+            <label>Lobby</label>
+            <input type="text" name="lobby" className="form-control" placeholder="N° du Lobby" value={this.state.name} onChange={this.saveNameHandler} />
           </div>
           <div className="form-group">
-            <label>N° du lobby</label>
-            <input type="text" name="number" className="form-control" placeholder="N° du Lobby" value={this.state.number} onChange={this.saveNumberHandler} />
+            <label>Nombre de joueurs</label>
+            <select className="form-control" value={this.state.number} onChange={this.handleNumberChange}>
+                        {
+                            this.createSelect()
+                        }
+                        </select>
           </div>
           <button type="submit" className="btn btn-success btn-block" onClick={this.createLobby}>Créer un Lobby</button>
         </form>
