75c58c8f328c924da9f9c77b21dff61113fb04f1
prynge <37028634+prynge@users.noreply.github.com>
2021-01-22 17:07:53 +0100

    added join function

--- src/vues/JoinLobbyComponent.jsx
+++ src/vues/JoinLobbyComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { configGame } from '../game/config'
 
 class JoinLobbyComponent extends Component {
   constructor(props) {
@@ -6,11 +7,25 @@ class JoinLobbyComponent extends Component {
     
     this.state = {
       name: '',
-      number: ''
+      number: '',
+      playerID: '',
+      lobbies: []
     }
   }
 
-  joinLobby = () =>{}
+  joinLobby = (e) =>{
+    e.preventDefault()
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ playerID: this.state.playerID, playerName: this.state.name })
+    };
+    fetch(`${configGame.httpORs}://${configGame.urlServer}/games/Jeu_Fil_Rouge/${this.state.number}/join`, requestOptions)
+        .then(response => response.json())
+        .then(data => 
+          this.props.history.push('/game', {
+            data: data}));
+  }
 
   saveNameHandler = (event) => {
     this.setState({ name: event.target.value });
@@ -20,8 +35,18 @@ class JoinLobbyComponent extends Component {
     this.setState({ number: event.target.value });
   }
 
+  lobbyShow(lobby) {
+    return (<li key={lobby.matchID} className='list-group list-group-item-action bg-danger'><button onClick={()=>this.setState({number:lobby.matchID, playerID: lobby.players.filter(player => (player.name == null))[0].id })}>{lobby.matchID}</button></li>)
+  }
+
+
   componentDidMount() {
-    
+    fetch (`${configGame.httpORs}://${configGame.urlServer}/games/Jeu_Fil_Rouge`)
+    .then(response => response.json())
+    .then( (data) => {
+      this.setState({lobbies : data.matches});
+      console.log(this.state.lobbies[0].players.filter(player => (player.name == null)));
+    });
   }
 
   render() {
@@ -31,15 +56,19 @@ class JoinLobbyComponent extends Component {
         <h2 className="text-center">Rejoindre un Lobby</h2>
         <form className="form-signup col-10 mx-auto" >
           <div className="form-group">
-            <label>N° du lobby</label>
-            <input type="text" name="name" className="form-control" placeholder="Veuillez saisir le code du Lobby" value={this.state.name} onChange={this.saveNameHandler} />
+            <label>Pseudo</label>
+            <input type="text" name="name" className="form-control" placeholder="Veuillez saisir un pseudo" value={this.state.name} onChange={this.saveNameHandler} />
           </div>
           <div className="form-group">
-            <label>Pseudo</label>
-            <input type="text" name="number" className="form-control" placeholder="Veuillez saisir un pseudo" value={this.state.number} onChange={this.saveNumberHandler} />
+            <label>Lobby</label>
+            <input type="text" name="number" className="form-control" placeholder="Veuillez saisir le code du Lobby" value={this.state.number} onChange={this.saveNumberHandler} />
           </div>
-          <button type="submit" className="btn btn-success btn-block" onClick={this.joinLobby}>Rejoindre le Lobby</button>
+          <button className="btn btn-success btn-block" onClick={this.joinLobby}>Rejoindre le Lobby</button>
         </form>
+        <p className='bg-secondary text-left mt-3'>lobby disponible :</p>  
+        <ul className='list-group mt-1'>
+          {this.state.lobbies.map((element) =>  this.lobbyShow(element))}
+        </ul>
       </div>
     );
   }
